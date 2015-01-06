c5ab5417410a1c8c6ad6b4f99d67ec225e55f914
Pram Attale <pram@attale.com>
2015-01-06 16:26:07 +0000

    ngFilters

--- src/main/web/main.js
+++ src/main/web/main.js
@@ -1,29 +1,19 @@
 var myApp = angular.module('myApp', []);
 
-myApp.factory('Data', function () {
-    return { message: "I'm data from a service" }
+myApp.factory('Avengers', function () {
+    var Avengers = {};
+    Avengers.cast = [
+        {
+            name: 'Tony Stark',
+            character: 'Iron Man'
+        }, {
+            name: 'Bruce Banner',
+            character: 'The Incredible Hulk'
+        }
+    ];
+    return Avengers;
 });
 
-myApp.filter('reverse', function () {
-    return function (text) {
-        return text.split("").reverse().join("");
-    }
-});
-
-myApp.filter('reverse2', function (Data) {
-    return function (text) {
-        return text.split("").reverse().join("") + Data.message;
-    }
-});
-
-function FirstCtrl($scope, Data) {
-    $scope.data = Data;
-}
-
-function SecondCtrl($scope, Data) {
-    $scope.data = Data;
-}
-
-function ThirdCtrl($scope, Data) {
-    $scope.data = Data;
+function AvengersCtrl($scope, Avengers) {
+    $scope.avengers = Avengers;
 }
