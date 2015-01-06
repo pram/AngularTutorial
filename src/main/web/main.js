072c70c5d8310ccf8ca7224791c5f3a0204367f8
Pram Attale <pram@attale.com>
2015-01-06 16:16:28 +0000

    filters

--- src/main/web/main.js
+++ src/main/web/main.js
@@ -4,14 +4,26 @@ myApp.factory('Data', function () {
     return { message: "I'm data from a service" }
 });
 
+myApp.filter('reverse', function () {
+    return function (text) {
+        return text.split("").reverse().join("");
+    }
+});
+
+myApp.filter('reverse2', function (Data) {
+    return function (text) {
+        return text.split("").reverse().join("") + Data.message;
+    }
+});
+
 function FirstCtrl($scope, Data) {
     $scope.data = Data;
 }
 
 function SecondCtrl($scope, Data) {
     $scope.data = Data;
+}
 
-    $scope.reversedMessage = function(message) {
-        return message.split("").reverse().join("");
-    };
+function ThirdCtrl($scope, Data) {
+    $scope.data = Data;
 }
