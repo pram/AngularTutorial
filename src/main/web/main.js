636ba2867e850ba03c6219ebff3c6fbe41b298f9
Pram Attale <pram@attale.com>
2015-01-06 08:42:51 +0000

    sharing data

--- src/main/web/main.js
+++ src/main/web/main.js
@@ -1,7 +1,13 @@
-function FirstCtrl($scope) {
+var myApp = angular.module('myApp', []);
 
+myApp.factory('Data', function () {
+    return { message: "I'm data from a service" };
+});
+
+function FirstCtrl($scope, Data) {
+    $scope.data = Data;
 }
 
-function SecondCtrl($scope) {
-
+function SecondCtrl($scope, Data) {
+    $scope.data = Data;
 }
