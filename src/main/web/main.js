ab91530581d26620520ff37f9b40bb33adec71ba
Pram Attale <pram@attale.com>
2015-01-07 08:13:53 +0000

    directives->controllers

--- src/main/web/main.js
+++ src/main/web/main.js
@@ -4,12 +4,15 @@ app.controller("AppCtrl", function ($scope) {
     $scope.loadMoreTweets = function () {
         alert("Loading tweets!");
     }
+    $scope.deleteTweets = function () {
+        alert("deleting tweets!");
+    }
 })
 
 app.directive("enter", function () {
     return function (scope, element, attrs) {
         element.bind("mouseenter", function () {
-            scope.loadMoreTweets();
+            scope.$apply(attrs.enter);
         })
     }
 })
