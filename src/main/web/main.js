2d6af836ac637e2e142ca80e2e98e1963e8400b7
Pram Attale <pram@attale.com>
2015-01-08 08:10:28 +0000

    controller organization

--- src/main/web/main.js
+++ src/main/web/main.js
@@ -1,68 +1,7 @@
-var app = angular.module("app",[]);
+var app = angular.module('phoneApp', []);
 
-app.directive("country", function () {
-    return {
-        restrict: "E",
-        controller: function () {
-            this.makeAnnouncement = function (message) {
-                console.log("Country says: " + message);
-            };
-        }
-    };
-});
-app.directive("state", function () {
-    return {
-        restrict: "E",
-        controller: function () {
-            this.makeLaw = function (law) {
-                console.log("Law: " + law);
-            };
-        }
-    };
-});
-app.directive("city", function () {
-    return {
-        restrict: "E",
-        require: ["^country","^state"],
-        link: function (scope, element, attrs, ctrls) {
-            ctrls[0].makeAnnouncement("This city rocks");
-            ctrls[1].makeLaw("Jump higher");
-        }
-    };
-});
-
-app.directive('draggable', function($document) {
-    return function(scope, element, attr) {
-        var startX = 0, startY = 0, x = 0, y = 0;
-        element.css({
-            position: 'relative',
-            border: '1px solid red',
-            backgroundColor: 'lightgrey',
-            cursor: 'pointer',
-            display: 'block',
-            width: '65px'
-        });
-        element.on('mousedown', function(event) {
-            // Prevent default dragging of selected content
-            event.preventDefault();
-            startX = event.screenX - x;
-            startY = event.screenY - y;
-            $document.on('mousemove', mousemove);
-            $document.on('mouseup', mouseup);
-        });
-
-        function mousemove(event) {
-            y = event.screenY - startY;
-            x = event.screenX - startX;
-            element.css({
-                top: y + 'px',
-                left:  x + 'px'
-            });
-        }
-
-        function mouseup() {
-            $document.off('mousemove', mousemove);
-            $document.off('mouseup', mouseup);
-        }
+app.controller("AppCtrl", function ($scope) {
+    $scope.sayHi = function () {
+        alert("hi");
     };
 });
